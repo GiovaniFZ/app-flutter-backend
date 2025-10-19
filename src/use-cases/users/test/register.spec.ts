65409ae4944bf88a2da10154805228e33828f518
GiovaniFZ <giovanifinazzi@gmail.com>
2025-10-18 21:09:01 -0300

    conf: test environment

---
+++ src/use-cases/users/test/register.spec.ts
@@ -0,0 +1,8 @@
+import { describe } from 'node:test';
+import { expect, test } from 'vitest';
+
+describe('should pass', () => {
+  test('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
